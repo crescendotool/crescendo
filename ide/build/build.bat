67ad0ce683ce95e1d28bc2134f00b8865bf1a42f
lausdahl <lausdahl@fd54fc82-ac30-4e97-bb06-279643433f24>
2010-09-10 18:38:49 +0000

    git-svn-id: svn://svn.code.sf.net/p/destecs/code/trunk@23 fd54fc82-ac30-4e97-bb06-279643433f24

--- ide/build/build.bat
+++ ide/build/build.bat
@@ -3,24 +3,14 @@ echo Building overture
 
 set BUILD_DIR=%CD%\build
 
-REM -Dbase=C:\Users\kela\Downloads\eclipse352
-REM -Dbaseos=win32
-REM -Dbasews=win32
-REM -Dbasearch=x86
-REM -DbuildDirectory=C:/overture.build2
 
 
-REM -DeclipseLocation=C:\Users\kela\Downloads\eclipse352
-REM -DequinoxLauncherPluginVersion=1.0.201.R35x_v20090715
-REM -DpdeBuildPluginVersion=3.5.2.R35x_20100114
-
-
-
-set ECLIPSE_HOME="C:\Users\kela\Downloads\eclipse352"
+set ECLIPSE_HOME="C:\Users\kela\Downloads\eclipse36"
 set BASE=%ECLIPSE_HOME%
-set BUILD_XML_PATH="C:\overture\overturesvn\ide\build\"
-set equinoxLauncherPluginVersion="1.0.201.R35x_v20090715"
-set pdeBuildPluginVersion="3.5.2.R35x_20100114"
+set BUILD_XML_PATH="C:\destecs\destecs_sf_svn\trunk\ide\build\"
+set OVERTURE_SOURCE_IDE_BASE="C:\overture\overturesvn\ide"
+set equinoxLauncherPluginVersion="1.1.0.v20100507"
+set pdeBuildPluginVersion="3.6.0.v20100603"
 set BASEOS=win32
 set BASEWS=win32
 set BASEARCH=x86
@@ -34,10 +24,11 @@ echo Starting Eclipse ANT runner
 
 set LAUNCHER_PLUGIN=%ECLIPSE_HOME%\plugins\org.eclipse.equinox.launcher_%equinoxLauncherPluginVersion%.jar
 
-java -cp %LAUNCHER_PLUGIN% org.eclipse.equinox.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile build.xml -Dbase=%BASE% -DeclipseLocation=%ECLIPSE_HOME% -DequinoxLauncherPluginVersion=%equinoxLauncherPluginVersion% -DbuildDirectory=%BUILD_DIR% -DpdeBuildPluginVersion=%pdeBuildPluginVersion% -Dbaseos=%BASEOS% -Dbasews=%BASEWS% -Dbasearch=%BASEARCH%
+java -cp %LAUNCHER_PLUGIN% org.eclipse.equinox.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile build.xml -Dbase=%BASE% -DeclipseLocation=%ECLIPSE_HOME% -DequinoxLauncherPluginVersion=%equinoxLauncherPluginVersion% -DbuildDirectory=%BUILD_DIR% -DpdeBuildPluginVersion=%pdeBuildPluginVersion% -Dbaseos=%BASEOS% -Dbasews=%BASEWS% -Dbasearch=%BASEARCH% -DOvertureSourceIdeBase=%OVERTURE_SOURCE_IDE_BASE%
 
 cd %BUILD_DIR%
 cd ..
 
 echo Done
+pause
 
