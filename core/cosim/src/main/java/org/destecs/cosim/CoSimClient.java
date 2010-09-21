4946a424fe6248c11c117a0b021677647ccc3763
augusto_silva <augusto_silva@fd54fc82-ac30-4e97-bb06-279643433f24>
2010-09-21 11:34:41 +0000

    cosim client with both simulators
    git-svn-id: svn://svn.code.sf.net/p/destecs/code/trunk@26 fd54fc82-ac30-4e97-bb06-279643433f24

--- core/cosim/src/main/java/org/destecs/cosim/CoSimClient.java
+++ core/cosim/src/main/java/org/destecs/cosim/CoSimClient.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -21,9 +22,10 @@ import org.destecs.protocol.structs.StepStruct;
 import org.destecs.protocol.structs.StepStructoutputsStruct;
 import org.destecs.protocol.structs.StepinputsStructParam;
 
-public class CoSimClient
-{
+public class CoSimClient {
 	public final static String MODEL_BASE_PATH = "C:\\destecs\\workspace\\watertank";
+
+	public final static String _20_sim_Machine = "http://localhost:1580";
 
 	/**
 	 * @param args
@@ -31,122 +33,271 @@ public class CoSimClient
 	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws MalformedURLException,
-			InterruptedException
-	{
+			InterruptedException {
 		String modelPath = MODEL_BASE_PATH;
-		if (args.length > 0)
-		{
+		if (args.length > 0) {
 			modelPath = args[0];
 			if (!new File(modelPath).exists()
-					|| !new File(modelPath).isDirectory())
-			{
+					|| !new File(modelPath).isDirectory()) {
 				System.err.println("Model path is not valid: " + modelPath);
 				return;
 			}
 		}
-		
-		List<D> simulationData =readData(args[1]);
-		
+
+		// List<D> simulationData =readData(args[1]);
+
+		// Connection to VDM
 		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
 		config.setServerURL(new URL("http://127.0.0.1:8080/xmlrpc"));
 		XmlRpcClient client = new XmlRpcClient();
 		client.setConfig(config);
 		AnnotationClientFactory factory = new AnnotationClientFactory(client);
 
-		ICoSimProtocol dt = (ICoSimProtocol) factory.newInstance(ICoSimProtocol.class);
+		ICoSimProtocol dt = (ICoSimProtocol) factory
+				.newInstance(ICoSimProtocol.class);
 
-		ProxyICoSimProtocol p = new ProxyICoSimProtocol(dt);
+		ProxyICoSimProtocol dtp = new ProxyICoSimProtocol(dt);
 
 		System.out.println("Interface Version: " + dt.getVersion());
 
-		if (!p.initialize().success)
-		{
+		if (!dtp.initialize().success) {
+			System.err.println("Initilization error");
+			return;
+		}
+		// Connection to VDM initialized
+
+		// THIS IS JUST DEBUG
+//		if (dtp.load(modelPath).success) {
+//			List<SetDesignParametersdesignParametersStructParam> shareadDesignParameters = new Vector<SetDesignParametersdesignParametersStructParam>();
+//			shareadDesignParameters
+//					.add(new SetDesignParametersdesignParametersStructParam(
+//							"minLevel", 1.0));
+//			shareadDesignParameters
+//					.add(new SetDesignParametersdesignParametersStructParam(
+//							"maxLevel", 2.0));
+//
+//			if (dtp.setDesignParameters(shareadDesignParameters).success
+//			// && ctp.setDesignParameters(shareadDesignParameters).success
+//			) {
+//				Double time = 0.0;
+//				Double level = 0.0;
+//				if (dtp.start().success) {
+//					for (int i = 0; i < 1000; i++) {
+//						List<StepinputsStructParam> inputs = new
+//						 Vector<StepinputsStructParam>();
+//						 inputs.add(new StepinputsStructParam("level", level));
+//						
+//						 List<String> events = new Vector<String>();
+//						 // if (time > 10 && eventLoop <= 0)
+//						 // {
+//						 // events.add("high");
+//						 // eventLoop = 5;
+//						 // }
+//						 // eventLoop--;
+//						 StepStruct result = dtp.step(time, inputs, false,
+//						 events);
+//					}
+//
+//				}
+//
+//			}
+//
+//			return;
+//		}
+
+		
+
+		// Connection to 20-sim
+		XmlRpcClientConfigImpl _20_sim_Config = new XmlRpcClientConfigImpl();
+		_20_sim_Config.setServerURL(new URL(_20_sim_Machine));
+		XmlRpcClient _20_sim_Client = new XmlRpcClient();
+		_20_sim_Client.setConfig(_20_sim_Config);
+		AnnotationClientFactory _20_sim_Factory = new AnnotationClientFactory(
+				_20_sim_Client);
+
+		ICoSimProtocol ct = (ICoSimProtocol) _20_sim_Factory
+				.newInstance(ICoSimProtocol.class);
+
+		ProxyICoSimProtocol ctp = new ProxyICoSimProtocol(ct);
+
+		// System.out.println("Interface Version: " + dt.getVersion());
+
+		if (!ctp.initialize().success) {
 			System.err.println("Initilization error");
 			return;
 		}
 
-		if (p.load(modelPath).success)
-		{
+		// Connection to 20-sim initialized
+
+		// List<StepinputsStructParam> ctInputs = new
+		// ArrayList<StepinputsStructParam>();
+		// ctInputs.add(new StepinputsStructParam("pipi", 3.14));
+		//
+		// for(int i=0;i<4;i++)
+		// ctp.step(new Double(1), ctInputs, true, new ArrayList<String>());
+
+		if (dtp.load(modelPath).success) {
 			/*
 			 * query the interface for matching / validation
 			 */
-			printInterface(p.queryInterface());
+			printInterface(dtp.queryInterface());
 
 			/*
-			 * well we should properly also set the shared design parameters here This should start the interpreter
+			 * well we should properly also set the shared design parameters
+			 * here This should start the interpreter
 			 */
-			// List<InitializeSharedParameterStructParam> shareadDesignParameters = new
+			// List<InitializeSharedParameterStructParam>
+			// shareadDesignParameters = new
 			// Vector<InitializeSharedParameterStructParam>();
-			// shareadDesignParameters.add(new InitializeSharedParameterStructParam("minLevel", 10.0));
-			// shareadDesignParameters.add(new InitializeSharedParameterStructParam("maxLevel", 500.0));
+			// shareadDesignParameters.add(new
+			// InitializeSharedParameterStructParam("minLevel", 10.0));
+			// shareadDesignParameters.add(new
+			// InitializeSharedParameterStructParam("maxLevel", 500.0));
 			//
-			// List<InitializefaultsStructParam> enabledFaults = new Vector<InitializefaultsStructParam>();
+			// List<InitializefaultsStructParam> enabledFaults = new
+			// Vector<InitializefaultsStructParam>();
 
 			List<SetDesignParametersdesignParametersStructParam> shareadDesignParameters = new Vector<SetDesignParametersdesignParametersStructParam>();
-			shareadDesignParameters.add(new SetDesignParametersdesignParametersStructParam("minLevel", 1.0));
-			shareadDesignParameters.add(new SetDesignParametersdesignParametersStructParam("maxLevel", 2.0));
+			shareadDesignParameters
+					.add(new SetDesignParametersdesignParametersStructParam(
+							"minLevel", 1.0));
+			shareadDesignParameters
+					.add(new SetDesignParametersdesignParametersStructParam(
+							"maxLevel", 2.0));
 
-			if (p.setDesignParameters(shareadDesignParameters).success)
-			{
+			if (dtp.setDesignParameters(shareadDesignParameters).success
+			// && ctp.setDesignParameters(shareadDesignParameters).success
+			) {
 
-				if (p.start().success)
-				{
+				if (dtp.start().success && ctp.start().success) {
 
+					// Initial input for DT
+					
+					Double initTime = 100.0;
 					Double time = 0.0;
 					Double level = 0.0;
-					int eventLoop = 5;
-					for (int i = 0; i < simulationData.size(); i++)
-					{
-						time = simulationData.get(i).timeS*1000;
-						level = simulationData.get(i).tankTankLevel;
-						/*
-						 * Begin stepping
-						 */
-						List<StepinputsStructParam> inputs = new Vector<StepinputsStructParam>();
-						inputs.add(new StepinputsStructParam("level", level));
+					List<String> events = new Vector<String>();
+					List<StepStructoutputsStruct> resultOutput = null;
 
-						List<String> events = new Vector<String>();
-//						if (time > 10 && eventLoop <= 0)
-//						{
-//							events.add("high");
-//							eventLoop = 5;
-//						}
-//						eventLoop--;
-						StepStruct result = p.step(time, inputs, false, events);
+					List<StepinputsStructParam> inputs = new Vector<StepinputsStructParam>();
+					inputs.add(new StepinputsStructParam("level", level));
 
-//						time = result.time;
+					StepStruct result = dtp.step(initTime, inputs, false, events);
+					
+					while (time < 5) {
+						// input for CT
+						resultOutput = result.outputs;
 
+						inputs = new Vector<StepinputsStructParam>();
+						for (StepStructoutputsStruct stepStructoutputsStruct : resultOutput) {
+
+							if (stepStructoutputsStruct.name
+									.equals("valveState")) {
+								inputs.add(new StepinputsStructParam(
+										stepStructoutputsStruct.name,
+										stepStructoutputsStruct.value));
+							}
+						}
+
+						result = ctp.step((result.time)/1000, inputs, false, events);
+
+				
+						
 						StringBuilder sb = new StringBuilder();
 
-						sb.append("VDM Requesting time step of = " + result.time + " ");
+						sb.append("20-sim Requesting time step of = "
+								+ result.time + " ");
 
-						for (StepStructoutputsStruct o : result.outputs)
-						{
+						for (StepStructoutputsStruct o : result.outputs) {
+							sb.append(o.name + " = " + o.value + " ");
+						}
+						
+						resultOutput = result.outputs;
+
+						System.out.println(sb.toString());
+
+						// input for DT
+						for (StepStructoutputsStruct stepStructoutputsStruct : resultOutput) {
+
+							if (stepStructoutputsStruct.name
+									.equals("level")) {
+								inputs.add(new StepinputsStructParam(
+										stepStructoutputsStruct.name,
+										stepStructoutputsStruct.value));
+							}
+						}
+						
+
+						
+						result = dtp.step(((result.time)*1000)+5, inputs, false,
+								events);
+
+						sb = new StringBuilder();
+
+						sb.append("VDM Requesting time step of = "
+								+ result.time + " ");
+
+						for (StepStructoutputsStruct o : result.outputs) {
 							sb.append(o.name + " = " + o.value + " ");
 						}
 
 						System.out.println(sb.toString());
-
-//						level += 0.1;
+						time = (result.time)/1000;
 					}
-					if (!p.stop().success)
-					{
+
+					// int eventLoop = 5;
+					// for (int i = 0; i < simulationData.size(); i++)
+					// {
+					// time = simulationData.get(i).timeS*1000;
+					// level = simulationData.get(i).tankTankLevel;
+					// /*
+					// * Begin stepping
+					// */
+					// List<StepinputsStructParam> inputs = new
+					// Vector<StepinputsStructParam>();
+					// inputs.add(new StepinputsStructParam("level", level));
+					//
+					// List<String> events = new Vector<String>();
+					// // if (time > 10 && eventLoop <= 0)
+					// // {
+					// // events.add("high");
+					// // eventLoop = 5;
+					// // }
+					// // eventLoop--;
+					// StepStruct result = dtp.step(time, inputs, false,
+					// events);
+					//
+					// // time = result.time;
+					//
+					// StringBuilder sb = new StringBuilder();
+					//
+					// sb.append("VDM Requesting time step of = " + result.time
+					// + " ");
+					//
+					// for (StepStructoutputsStruct o : result.outputs)
+					// {
+					// sb.append(o.name + " = " + o.value + " ");
+					// }
+					//
+					// System.out.println(sb.toString());
+
+					// level += 0.1;
+					// }
+
+					if (!dtp.stop().success) {
 						System.err.println("Stop faild");
 					}
-				} else
-				{
+				} else {
 					System.err.println("Initilization faild");
 				}
-			} else
-			{
+			} else {
 				System.err.println("setDesignParameters faild");
 			}
-		} else
-		{
+		} else {
 			System.err.println("Load faild");
 		}
 
-		p.terminate();
+		dtp.terminate();
 
 		// if (p.load("C:\\destecs\\workspace\\watertank").success)
 		// {
@@ -156,14 +307,19 @@ public class CoSimClient
 		// printInterface(p.queryInterface());
 		//
 		// /*
-		// * well we should properly also set the shared design parameters here This should start the interpreter
+		// * well we should properly also set the shared design parameters here
+		// This should start the interpreter
 		// */
-		// List<InitializeSharedParameterStructParam> shareadDesignParameters = new
+		// List<InitializeSharedParameterStructParam> shareadDesignParameters =
+		// new
 		// Vector<InitializeSharedParameterStructParam>();
-		// shareadDesignParameters.add(new InitializeSharedParameterStructParam("minLevel", 10.0));
-		// shareadDesignParameters.add(new InitializeSharedParameterStructParam("maxLevel", 500.0));
+		// shareadDesignParameters.add(new
+		// InitializeSharedParameterStructParam("minLevel", 10.0));
+		// shareadDesignParameters.add(new
+		// InitializeSharedParameterStructParam("maxLevel", 500.0));
 		//
-		// List<InitializefaultsStructParam> enabledFaults = new Vector<InitializefaultsStructParam>();
+		// List<InitializefaultsStructParam> enabledFaults = new
+		// Vector<InitializefaultsStructParam>();
 		// if (p.initialize(shareadDesignParameters, enabledFaults).success)
 		// {
 		//
@@ -174,7 +330,8 @@ public class CoSimClient
 		// /*
 		// * Begin stepping
 		// */
-		// List<StepinputsStructParam> inputs = new Vector<StepinputsStructParam>();
+		// List<StepinputsStructParam> inputs = new
+		// Vector<StepinputsStructParam>();
 		// inputs.add(new StepinputsStructParam("level", level));
 		//
 		// StepStruct result = p.step(time, inputs, false);
@@ -208,17 +365,14 @@ public class CoSimClient
 		// }
 	}
 
-	public static String pad(String text, int length)
-	{
-		while (text.length() < length)
-		{
+	public static String pad(String text, int length) {
+		while (text.length() < length) {
 			text += " ";
 		}
 		return text;
 	}
 
-	public static void printInterface(QueryInterfaceStruct result)
-	{
+	public static void printInterface(QueryInterfaceStruct result) {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append("_____________________________\n");
@@ -227,86 +381,70 @@ public class CoSimClient
 
 		sb.append("|  Shared Design Parameters\n");
 		sb.append("|\n");
-		if (result.sharedDesignParameters.size() > 0)
-		{
-			for (String p : result.sharedDesignParameters)
-			{
+		if (result.sharedDesignParameters.size() > 0) {
+			for (String p : result.sharedDesignParameters) {
 				sb.append("|    " + p/* p.name + " : " + p.value */+ "\n");
 			}
-		} else
-		{
+		} else {
 			sb.append("|    None.\n");
 		}
 		sb.append("|----------------------------\n");
 		sb.append("|  Input Variables\n");
 		sb.append("|\n");
-		if (result.inputs.size() > 0)
-		{
-			for (String p : result.inputs)
-			{
+		if (result.inputs.size() > 0) {
+			for (String p : result.inputs) {
 				sb.append("|    " + p /* p.name + " : " + p.value */+ "\n");
 			}
-		} else
-		{
+		} else {
 			sb.append("|    None.\n");
 		}
 		sb.append("|----------------------------\n");
 		sb.append("|  Output Variables\n");
 		sb.append("|\n");
-		if (result.outputs.size() > 0)
-		{
-			for (String p : result.outputs)
-			{
+		if (result.outputs.size() > 0) {
+			for (String p : result.outputs) {
 				sb.append("|    " + p/* p.name + " : " + p.value */+ "\n");
 			}
-		} else
-		{
+		} else {
 			sb.append("|    None.\n");
 		}
 		sb.append("_____________________________");
 		System.out.println(sb.toString());
 	}
 
-	public static List<D> readData(String filename)
-	{
+	public static List<D> readData(String filename) {
 		List<D> data = new Vector<D>();
 
 		File file = new File(filename);
 		StringBuffer contents = new StringBuffer();
 		BufferedReader reader = null;
 
-		try
-		{
+		try {
 			reader = new BufferedReader(new FileReader(file));
 			String text = null;
 			boolean isFirst = true;
 			// repeat until all lines is read
-			while ((text = reader.readLine()) != null)
-			{
-				if (isFirst)
-				{
+			while ((text = reader.readLine()) != null) {
+				if (isFirst) {
 					isFirst = false;
 					continue;
 				}
 				String[] elements = text.split(",");
-				data.add(new D(Double.parseDouble(elements[0]), Double.parseDouble(elements[1]), Double.parseDouble(elements[2]), Double.parseDouble(elements[3])));
+				data.add(new D(Double.parseDouble(elements[0]), Double
+						.parseDouble(elements[1]), Double
+						.parseDouble(elements[2]), Double
+						.parseDouble(elements[3])));
 			}
-		} catch (FileNotFoundException e)
-		{
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
-		} finally
-		{
-			try
-			{
-				if (reader != null)
-				{
+		} finally {
+			try {
+				if (reader != null) {
 					reader.close();
 				}
-			} catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
@@ -314,8 +452,7 @@ public class CoSimClient
 		return data;
 	}
 
-	public static class D
-	{
+	public static class D {
 		// time{s},tank\Tank\level,Control\valvecontrol,Control\count
 		public final Double timeS;
 		public final Double tankTankLevel;
@@ -323,18 +460,19 @@ public class CoSimClient
 		public final Double controlCount;
 
 		public D(Double timeS, Double tankTankLevel,
-				Double controlValveControl, Double controlCount)
-		{
+				Double controlValveControl, Double controlCount) {
 			this.timeS = timeS;
 			this.controlCount = controlCount;
 			this.controlValveControl = controlValveControl;
 			this.tankTankLevel = tankTankLevel;
 		}
-		
+
 		@Override
-		public String toString()
-		{
-			return String.format("\ntimeS=%1$s, \ntankTankLevel=%2$s, \ncontrolValveControl=%3$s, \ncontrolCount=%4$s", timeS, tankTankLevel, controlValveControl, controlCount);
+		public String toString() {
+			return String
+					.format("\ntimeS=%1$s, \ntankTankLevel=%2$s, \ncontrolValveControl=%3$s, \ncontrolCount=%4$s",
+							timeS, tankTankLevel, controlValveControl,
+							controlCount);
 		}
 	}
 }
