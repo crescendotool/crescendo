d9d67cae6eb8cb180bac7e46c26486431e36be3a
augusto_silva <augusto_silva@fd54fc82-ac30-4e97-bb06-279643433f24>
2010-09-22 15:08:14 +0000

    simulation Engine viewer
    git-svn-id: svn://svn.code.sf.net/p/destecs/code/trunk@32 fd54fc82-ac30-4e97-bb06-279643433f24

---
+++ ide/simulationEngine/ui/src/main/java/org/destecs/ide/simeng/ui/CoSimStarterView.java
@@ -0,0 +1,22 @@
+package org.destecs.ide.simeng.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class CoSimStarterView extends ViewPart {
+
+	public CoSimStarterView() {
+		super();
+	}
+
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
