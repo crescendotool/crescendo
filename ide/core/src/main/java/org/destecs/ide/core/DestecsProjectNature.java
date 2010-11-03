516719cec33ac33274060361d370ec55d07dcf1b
augusto_silva <augusto_silva@fd54fc82-ac30-4e97-bb06-279643433f24>
2010-11-03 10:18:08 +0000

    Added DESTECS nature
    git-svn-id: svn://svn.code.sf.net/p/destecs/code/trunk@85 fd54fc82-ac30-4e97-bb06-279643433f24

---
+++ ide/core/src/main/java/org/destecs/ide/core/DestecsProjectNature.java
@@ -0,0 +1,30 @@
+package org.destecs.ide.core;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class DestecsProjectNature implements IProjectNature {
+
+	protected IProject project = null;
+	
+	public IProject getProject() {		
+		return project;
+	}
+
+	public void setProject(IProject project) {
+		this.project = project;
+
+	}
+
+	public void configure() throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deconfigure() throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
