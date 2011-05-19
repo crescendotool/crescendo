733025147a49e40c1594831ae0d2e16fc0e752ac
augusto_silva <augusto_silva@fd54fc82-ac30-4e97-bb06-279643433f24>
2011-05-19 13:04:29 +0000

    git-svn-id: svn://svn.code.sf.net/p/destecs/code/trunk@247 fd54fc82-ac30-4e97-bb06-279643433f24

--- ide/debug/src/main/java/org/destecs/ide/debug/launching/ui/internal/_20simHelper.java
+++ ide/debug/src/main/java/org/destecs/ide/debug/launching/ui/internal/_20simHelper.java
@@ -2,8 +2,6 @@ package org.destecs.ide.debug.launching.ui.internal;
 
 import java.net.URL;
 
-import org.apache.xmlrpc.client.XmlRpcClient;
-import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.destecs.core.simulationengine.SimulationEngine;
 import org.destecs.core.simulationengine.exceptions.SimulationException;
 import org.destecs.core.simulationengine.xmlrpc.client.CustomSAXParserTransportFactory;
@@ -17,27 +15,27 @@ public class _20simHelper {
 			throws SimulationException {
 		ProxyICoSimProtocol protocolProxy = null;
 		try {
-			XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-			config.setServerURL(url);
-			// config.setReplyTimeout(5000);// 5 sec time out
-
-			XmlRpcClient client = new XmlRpcClient();
-			client.setConfig(config);
-
-			if (SimulationEngine.eclipseEnvironment) {
-				client.setTransportFactory(new CustomSAXParserTransportFactory(
-						client));
-			}
-
-			// clients.add(client);
-			// add factory for annotations for generated protocol
-			AnnotationClientFactory factory = new AnnotationClientFactory(
-					client);
-
-			ICoSimProtocol protocol = (ICoSimProtocol) factory
-					.newInstance(ICoSimProtocol.class);
-
-			protocolProxy = new ProxyICoSimProtocol(protocol);
+//			XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+//			config.setServerURL(url);
+//			// config.setReplyTimeout(5000);// 5 sec time out
+//
+//			XmlRpcClient client = new XmlRpcClient();
+//			client.setConfig(config);
+//
+//			if (SimulationEngine.eclipseEnvironment) {
+//				client.setTransportFactory(new CustomSAXParserTransportFactory(
+//						client));
+//			}
+//
+//			// clients.add(client);
+//			// add factory for annotations for generated protocol
+//			AnnotationClientFactory factory = new AnnotationClientFactory(
+//					client);
+//
+//			ICoSimProtocol protocol = (ICoSimProtocol) factory
+//					.newInstance(ICoSimProtocol.class);
+//
+//			protocolProxy = new ProxyICoSimProtocol(protocol);
 		} catch (Exception e) {
 
 		}
