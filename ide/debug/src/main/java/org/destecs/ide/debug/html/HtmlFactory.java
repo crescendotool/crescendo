53c3708d9edda2a5a510a697e2ac4f0c009c6b7d
lausdahl <lausdahl@fd54fc82-ac30-4e97-bb06-279643433f24>
2011-12-13 10:07:57 +0000

    improved the html page made for aca
    git-svn-id: svn://svn.code.sf.net/p/destecs/code/trunk@424 fd54fc82-ac30-4e97-bb06-279643433f24

--- ide/debug/src/main/java/org/destecs/ide/debug/html/HtmlFactory.java
+++ ide/debug/src/main/java/org/destecs/ide/debug/html/HtmlFactory.java
@@ -3,6 +3,7 @@ package org.destecs.ide.debug.html;
 import java.util.List;
 import java.util.Map;
 
+import org.destecs.ide.debug.IDebugConstants;
 import org.destecs.ide.debug.core.model.internal.DestecsDebugTarget;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.DebugException;
@@ -15,7 +16,7 @@ public class HtmlFactory
 		StringBuilder sb = new StringBuilder();
 
 		sb.append("<html>\n");
-		sb.append("<header>"+style+"</header>\n");
+		sb.append("<header>" + style + "</header>\n");
 		sb.append("<body>\n");
 
 		sb.append("<h1>ACA Simulation - " + name + "</h1>\n");
@@ -24,8 +25,9 @@ public class HtmlFactory
 		{
 			try
 			{
-				sb.append("<h2>" + target.getOutputFolder().getName()
-						+ "</h2>\n");
+				sb.append("<h2><a href=\"" + target.getOutputFolder().getName()
+						+ "\">" + target.getOutputFolder().getName()
+						+ "</a></h2>\n");
 
 				@SuppressWarnings("unchecked")
 				Map<Object, Object> attributes = target.getLaunch().getLaunchConfiguration().getAttributes();
@@ -38,9 +40,33 @@ public class HtmlFactory
 				boolean isEven = true;
 				for (Map.Entry<Object, Object> entry : attributes.entrySet())
 				{
-					sb.append("<tr "+(isEven?"class=\"alt\"":"")+">\n");
+					sb.append("<tr " + (isEven ? "class=\"alt\"" : "") + ">\n");
 					sb.append("\t<td>" + entry.getKey() + "</td>\n");
-					sb.append("\t<td>" + entry.getValue() + "</td>\n");
+					sb.append("\t<td>");
+					if (entry.getKey().equals(IDebugConstants.DESTECS_LAUNCH_CONFIG_VDM_LOG_VARIABLES)
+							|| entry.getKey().equals(IDebugConstants.DESTECS_LAUNCH_CONFIG_20SIM_LOG_VARIABLES)
+							|| entry.getKey().equals(IDebugConstants.DESTECS_LAUNCH_CONFIG_SHARED_DESIGN_PARAM))
+					{
+						sb.append("\t\t<ul>\n");
+
+						String splitter = ",";
+						if(entry.getKey().equals(IDebugConstants.DESTECS_LAUNCH_CONFIG_SHARED_DESIGN_PARAM))
+						{
+							splitter = ";";
+						}
+						
+						String[] items = entry.getValue().toString().split(splitter);
+						for (String string : items)
+						{
+							sb.append("\t\t\t<li>" + string + "</li>\n");
+						}
+
+						sb.append("\t\t</ul>\n");
+					} else
+					{
+						sb.append(entry.getValue());
+					}
+					sb.append("</td>\n");
 					sb.append("</tr>\n");
 					isEven = !isEven;
 				}
@@ -67,7 +93,7 @@ public class HtmlFactory
 
 		return sb.toString();
 	}
-	
+
 	private static final String style = "<style type=\"text/css\">#customers{font-family:\"Trebuchet MS\", Arial, Helvetica, sans-serif;width:100%;border-collapse:collapse;}#customers td, #customers th{font-size:1em;border:1px solid #98bf21;padding:3px 7px 2px 7px;}#customers th{font-size:1.1em;text-align:left;padding-top:5px;padding-bottom:4px;background-color:#A7C942;color:#ffffff;}#customers tr.alt td{color:#000000;background-color:#EAF2D3;}</style>";
 
 }
