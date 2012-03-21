72dfff70564e613bb09704d22cd692215872bea5
lausdahl <lausdahl@fd54fc82-ac30-4e97-bb06-279643433f24>
2012-03-21 14:44:35 +0000

    updated vdm version
    git-svn-id: svn://svn.code.sf.net/p/destecs/code/trunk@592 fd54fc82-ac30-4e97-bb06-279643433f24

--- core/vdm/src/main/java/org/destecs/vdm/CoSimImpl.java
+++ core/vdm/src/main/java/org/destecs/vdm/CoSimImpl.java
@@ -77,7 +77,7 @@ public class CoSimImpl implements IDestecs
 	public static final String LOAD_SETTING_DISABLE_RT_LOG = "settings_disable_rt_log";
 	public static final String LOAD_SETTING_DISABLE_RT_VALIDATOR = "settings_disable_rt_validator";
 
-	private static final String version = "1.0.0.0";
+	private static final String version = "1.0.0.1";
 	private static final String LOAD_SETTING_LOG_VARIABLES = "settings_log_variables";
 	private static final String LOAD_SETTING_DISABLE_COVERAGE = "settings_disable_coverage";
 	private String interfaceVersion = "3.0.3.0";
